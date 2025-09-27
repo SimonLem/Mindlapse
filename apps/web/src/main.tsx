67cb1970cc52ebbcdf200c77c275c9f375240f02
Simon Lemaitre <sim.lemaitre@gmail.com>
2025-09-27 19:53:11 +0200

    Ajout de shadcn + axios + test première route

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,27 +1,34 @@
-import React from "react";
+import "@repo/ui/globals.css";
 import { createRoot } from "react-dom/client";
-import "./style.css";
+import { useEffect } from "react";
+import api from "../lib/api";
 import typescriptLogo from "/typescript.svg";
-import { Header, Counter } from "@repo/ui";
+import { Button } from "@repo/ui/components/ui/button";
 
-const App = () => (
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" className="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img
-        src={typescriptLogo}
-        className="logo vanilla"
-        alt="TypeScript logo"
-      />
-      <p>ok</p>
-    </a>
-    <Header title="Web" />
-    <div className="card">
-      <Counter />
+const App = () => {
+  useEffect(() => {
+    api
+      .get("/")
+      .then((res: any) => console.log(res))
+      .catch((err: any) => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://www.typescriptlang.org/" target="_blank">
+        <img
+          src={typescriptLogo}
+          className="logo vanilla"
+          alt="TypeScript logo"
+        />
+        <p>ok</p>
+      </a>
+      <Button>Button</Button>
     </div>
-  </div>
-);
+  );
+};
 
 createRoot(document.getElementById("app")!).render(<App />);
